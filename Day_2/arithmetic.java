0c12e9b7988980cf161a112911412709bfaf2418
Atharv Naik <114814921+atharvnaik07@users.noreply.github.com>
2023-07-14 22:52:11 +0530

    Done

---
+++ Day_2/arithmetic.java
@@ -0,0 +1,16 @@
+public class sum {
+    public static void main(String[] args) {
+        int a=15 ;
+        int b=55;
+        int c= a + b;
+        System.out.println(c);
+        c=a-b;
+        System.out.println(c);
+        c=a*b;
+        System.out.println(c);
+        c=a/b;
+        System.out.println(c);
+        c=a%b;
+        System.out.println(c);
+    }
+}
---
+++ Day_2/relation.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+        int a=15 ;
+        int b=55;
+        if(a<b)
+            System.out.println(a+" is greater than "+b);
+        else
+            System.out.println(a+" is less than "+b);
+    }
+}
